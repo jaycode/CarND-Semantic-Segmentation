6bbfc49a6514e9183febb205cdf95bab1e34df52
Jay <teguhwpurwanto@gmail.com>
2017-12-07 15:52:37 -0800

    init: Replace with toothpick project

--- helper.py
+++ helper.py
@@ -10,7 +10,8 @@ import tensorflow as tf
 from glob import glob
 from urllib.request import urlretrieve
 from tqdm import tqdm
-
+import cv2
+import imutils
 
 class DLProgress(tqdm):
     last_block = 0
@@ -58,7 +59,7 @@ def maybe_download_pretrained_vgg(data_dir):
         os.remove(os.path.join(vgg_path, vgg_filename))
 
 
-def gen_batch_function(data_folder, image_shape):
+def gen_batch_function_original(data_folder, image_shape):
     """
     Generate function to create batches of training data
     :param data_folder: Path to folder that contains all the datasets
@@ -88,14 +89,153 @@ def gen_batch_function(data_folder, image_shape):
                 gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
 
                 gt_bg = np.all(gt_image == background_color, axis=2)
+                print (gt_bg.shape)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                print (gt_bg.shape)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+                print (gt_image.shape)
 
+                print (gt_image)
                 images.append(image)
                 gt_images.append(gt_image)
 
             yield np.array(images), np.array(gt_images)
     return get_batches_fn
+
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
+def apply_transformation(image, image_shape):
+    global fgbg, kernel
+    if image_shape is not None:
+        image = scipy.misc.imresize(image, image_shape)
+    fgmask = fgbg.apply(image)
+    dilated = cv2.dilate(fgmask, kernel, iterations=5)
+    dilated = cv2.erode(dilated, kernel, iterations=5)
+
+    cnts = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+    mask = np.zeros_like(image) # Create mask where white is what we want, black otherwise
+    mask = cv2.drawContours(mask, cnts, -1, color=(255, 255, 255), thickness=cv2.FILLED) # Draw filled contour in mask
+    out = np.zeros_like(image) # Extract out the object and place into output image
+    out[mask == 255] = image[mask == 255]
+
+    label = np.array([[False]*image.shape[1]]*image.shape[0])
+    boolmask = (mask == [255, 255, 255]).all(axis=-1)
+    label[boolmask] = True
+    return label
+
+def process_image(image, images, labels):
+    import numpy as np
+    #image[(image[:,:,0] >100) & (image[:,:,1] > 100) & (image[:,:,2] > 100)] = 0
+    #pylab.imshow(image)
+    #pylab.show()
+    # image_shape = (135, 240)
+    image_shape = (160, 576)
+    image_r = scipy.misc.imresize(image, image_shape)
+    #pylab.imshow(image_r)
+    #label = np.zeros_like(image_r)
+    #background_color = np.array([255, 0, 0])
+    #label[:,:] = background_color
+    # threshold = ((image_r[:,:,0] < 100) | (image_r[:,:,1] < 100) | (image_r[:,:,2] < 100))
+    threshold = apply_transformation(image, image_shape)
+
+    #print(threshold.shape)
+    threshold = threshold.reshape(*threshold.shape, 1)
+    #print (threshold.shape)
+    label = np.concatenate((threshold, np.invert(threshold)), axis=2)
+    images.append(image_r)
+    labels.append(label)
+
+def gen_batch_function(data_folder, image_shape):
+    """
+    Generate function to create batches of training data
+    :param data_folder: Path to folder that contains all the datasets
+    :param image_shape: Tuple - Shape of image
+    :return:
+    """
+    def get_batches_fn(batch_size):
+        """
+        Create batches of training data
+        :param batch_size: Batch Size
+        :return: Batches of training data
+        """
+        
+        import pylab
+        import imageio
+        filename = 'toothpick.mp4'
+        vid = imageio.get_reader(filename,  'ffmpeg')
+        images = []
+        labels = []
+        # print("fps:", vid.get_meta_data()['fps'])
+        for i in range(200):
+            image = vid.get_data(i)
+        # for i, image in enumerate(vid): #350  
+            #print('Mean of frame %i is %1.1f' % (i, im.mean()))
+            # image = vid.get_data(i)
+        # i = 0
+        # frame_exists = True
+        # while frame_exists:
+        #     try:
+        #         image = vid.get_data(i)
+        #     except:
+        #         frame_exists = False
+        #     i += 1
+            #fig = pylab.figure()
+            #fig.suptitle('image #{}'.format(num), fontsize=20)
+            #pylab.imshow(image)
+            process_image(image, images, labels)
+
+        #random.shuffle(image_paths)
+        for batch_i in range(0, len(images), batch_size):
+            #print('batch ' + str(batch_i))
+            print("total images:")
+            print(len(images[batch_i:batch_i+batch_size]))
+            yield images[batch_i:batch_i+batch_size], labels[batch_i:batch_i+batch_size]
+    return get_batches_fn
+
+
+def get_validation_set():
+    import pylab
+    import imageio
+    filename = 'toothpick2.mp4'
+    vid = imageio.get_reader(filename,  'ffmpeg')
+    val_images = []
+    val_labels = []
+    # for i, image in enumerate(vid):
+    for i in range(10):
+        image = vid.get_data(i)
+    # i = 0
+    # frame_exists = True
+    # while frame_exists:
+    #     try:
+    #         image = vid.get_data(i)
+    #     except:
+    #         frame_exists = False
+    #     i += 1
+        process_image(image, val_images, val_labels)
+    return val_images, val_labels
+
+def get_inference_set():
+    import pylab
+    import imageio
+    filename = 'toothpick3.mp4'
+    vid = imageio.get_reader(filename,  'ffmpeg')
+    images = []
+    labels = []
+    # for i, image in enumerate(vid):
+    for i in range(10):
+        image = vid.get_data(i)
+    # i = 0
+    # frame_exists = True
+    # while frame_exists:
+    #     try:
+    #         image = vid.get_data(i)
+    #     except:
+    #         frame_exists = False
+    #     i += 1
+        process_image(image, images, labels)
+    return images
 
 
 def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
@@ -109,9 +249,13 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
     :param image_shape: Tuple - Shape of image
     :return: Output for for each test image
     """
-    for image_file in glob(os.path.join(data_folder, 'image_2', '*.png')):
-        image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-
+    
+    images = get_inference_set()
+    #for image_file in glob(os.path.join(data_folder, 'image_2', '*.png')):
+    for i in range(len(images)):
+        #image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+        image = images[i]
+        image_file = 'frame_' + str(i) + '.png'
         im_softmax = sess.run(
             [tf.nn.softmax(logits)],
             {keep_prob: 1.0, image_pl: [image]})
@@ -137,4 +281,5 @@ def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_p
     image_outputs = gen_test_output(
         sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
     for name, image in image_outputs:
+        print(name)
         scipy.misc.imsave(os.path.join(output_dir, name), image)
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
-import time
 
 
 # Check TensorFlow Version
@@ -35,18 +34,32 @@ def load_vgg(sess, vgg_path):
     vgg_layer7_out_tensor_name = 'layer7_out:0'
 
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
-    graph = tf.get_default_graph()
-    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
-    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
-    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
-    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
-    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
-    
-    return w1, keep, layer3_out, layer4_out, layer7_out
+    return tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name),\
+           tf.get_default_graph().get_tensor_by_name(vgg_keep_prob_tensor_name),\
+           tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name),\
+           tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name),\
+           tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
+           
 tests.test_load_vgg(load_vgg, tf)
 
+def crop_and_add(x1,x2):
+    x1_shape = tf.shape(x1)
+    x2_shape = tf.shape(x2)
+    # offsets for the top left corner of the crop
+    offsets = [0, (x1_shape[1] - x2_shape[1]) // 2, (x1_shape[2] - x2_shape[2]) // 2, 0]
+    size = [-1, x2_shape[1], x2_shape[2], -1]
+    x1_crop = tf.slice(x1, offsets, size)
+    return x1_crop + x2
 
-def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+def crop_image(tensor, new_shape):
+    t_shape = tf.shape(tensor)
+    # offsets for the top left corner of the crop
+    offsets = [0, (t_shape[1] - new_shape[0]) // 2, (t_shape[2] - new_shape[1]) // 2, 0]
+    size = [-1, new_shape[0], new_shape[1], -1]
+    t1_crop = tf.slice(tensor, offsets, size)
+    return t1_crop
+
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes, image_shape):
     """
     Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
     :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
@@ -56,25 +69,22 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    layer7_conv = tf.layers.conv2d(vgg_layer7_out, 512, 1, padding='SAME',
-                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer4_up = tf.layers.conv2d_transpose(layer7_conv, 512, 2, 2,
-                                           kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer4_conv = tf.layers.conv2d(vgg_layer4_out, 512, 1,
-                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer4_out = tf.add(layer4_up, layer4_conv)
-    # -- 
-    layer3_up = tf.layers.conv2d_transpose(layer4_out, 256, 2, 2,
-                                           kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer3_conv = tf.layers.conv2d(vgg_layer3_out, 256, 1,
-                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer3_out = tf.add(layer3_up, layer3_conv)
-    output = tf.layers.conv2d_transpose(layer3_out, num_classes, 8, strides=8, padding='SAME',
-                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    net = {}
+    net['conv_1x1'] = tf.layers.conv2d(vgg_layer7_out, 512, 1, strides=(1,1))    #1x1 conv
+    net['upsampling_1'] = tf.layers.conv2d_transpose(net['conv_1x1'], 512, 2, strides=(2, 2)) #upsampling
+   
+    #paddings = tf.constant([[0, 0], [1, 0], [0, 0], [0, 0]])
+    #vgg_layer4_out = tf.pad(vgg_layer4_out, paddings, 'CONSTANT')
+   
+    net['fuse_vgg4'] = crop_and_add(net['upsampling_1'], vgg_layer4_out)                                     #skip connection
+    net['upsampling_2'] = tf.layers.conv2d_transpose(net['fuse_vgg4'], 256, 2, strides=(2, 2)) #upsampling
+    net['fuse_vgg3'] = crop_and_add(net['upsampling_2'], vgg_layer3_out)                                     #skip connection
+    net['upsampling_3'] = tf.layers.conv2d_transpose(net['fuse_vgg3'], num_classes, 8, strides=(8, 8))#upsampling
+    
+    net['upsampling_3'] = crop_image(net['upsampling_3'], image_shape)
 
-
-    return output
-tests.test_layers(layers)
+    return net['upsampling_3'], net
+#tests.test_layers(layers)
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -88,13 +98,13 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
-    cross_entropy_loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
-    train_op = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy_loss)
-
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    train_op = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_loss)
     return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
+
+import time
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
@@ -112,34 +122,52 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    for epoch in range(epochs):
-        print("epoch: ", epoch)
-        batch = 0
-        for images, labels in get_batches_fn(batch_size):
-            # Training
-            start = time.time()
-            _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={input_image:images,
-                                          correct_label:labels,
-                                          keep_prob:1.0})
-            end = time.time()
-            print('batch = ', batch, ', loss = ', loss, ', time = ', end-start)
-            batch += 1
-    pass
+    # image_shape = (135, 240)
+    image_shape = (160, 576)
+    data_dir = './data'
+    get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    
+    #print('\n'.join([n.name for n in tf.get_default_graph().as_graph_def().node]))
+    val_images, val_labels = helper.get_validation_set()
+    for i in range(epochs):
+      print('epoch: ', i)
+      #print('epoch: ', i, end='', flush=True)  #no newline
+      gen = get_batches_fn(batch_size)
+      batch = 0
+      for images, labels in gen:
+        start = time.time()  
+        _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image:images, correct_label:labels, keep_prob:1.0})
+        end = time.time()
+        print('batch = ', batch, ', loss = ', loss, ', time = ', end-start)
+        val_loss = sess.run([cross_entropy_loss], feed_dict={input_image:val_images, correct_label:val_labels, keep_prob:1.0})
+        print('val_loss = ', val_loss)
+        batch += 1
+      
+    # Save the variables to disk.
+    #saver = tf.train.Saver()
+    #save_path = saver.save(sess, "model.ckpt")
+    #print("Model saved in file: %s" % save_path)     
+      
 tests.test_train_nn(train_nn)
+
 
 
 def run():
     num_classes = 2
-    image_shape = (160, 576)
+    image_shape = (270, 480)
+    # image_shape = (135, 240)
+    # image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
-    tests.test_for_kitti_dataset(data_dir)
-    correct_label = tf.placeholder(tf.float32, shape=[None, image_shape[0], image_shape[1], num_classes])
-    epochs = 30
-    batch_size=32
-    learning_rate=1e-5
-
+    # tests.test_for_kitti_dataset(data_dir)
+    epochs = 1
+    batch_size = 1
+    learning_rate = 1e-4
+    correct_label = tf.placeholder(tf.float32, shape = [None, image_shape[0], image_shape[1], num_classes])
+    
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
 
@@ -147,28 +175,49 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    #config = tf.ConfigProto(
+    #    device_count = {'GPU': 0}
+    #)
+    
+    #with tf.Session(config=config) as sess:
+   
     with tf.Session() as sess:
-        # Path to vgg model
-        vgg_path = os.path.join(data_dir, 'vgg')
+
+    #sv = tf.train.Supervisor(logdir='.', save_model_secs=60)
+    #with sv.managed_session() as sess:       
+        vgg_path = os.path.join(data_dir, 'vgg')# Path to vgg model
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
+        #print(next(get_batches_fn(batch_size)))
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
-        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
-        logits, train_op, cross_entropy_loss = optimize(layer_output,
-                                                        correct_label,
-                                                        learning_rate,
-                                                        num_classes)
+        input_image, keep_prob, vgg_layer3, vgg_layer4, vgg_layer7 = load_vgg(sess, vgg_path)
+        nn_last_layer, net = layers(vgg_layer3, vgg_layer4, vgg_layer7, num_classes, image_shape)
+        print("shape:") 
+        print(vgg_layer7.get_shape()) 
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+        
+        gen = get_batches_fn(batch_size)
+        images, labels = next(gen)
 
-        # TODO: Train NN using the train_nn function
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
-        train_nn(sess, epochs, batch_size, get_batches_fn, train_op,cross_entropy_loss, input_image,
+        ret = sess.run([net[k] for k in net], feed_dict={input_image:images, correct_label:labels, keep_prob:1.0})
+        for r in ret:
+          print(r.shape)
+        
+        saver = tf.train.Saver()
+        # TODO: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, 
+                 cross_entropy_loss, input_image,
                  correct_label, keep_prob, learning_rate)
+        
+        # Save the variables to disk.
+        save_path = saver.save(sess, "./model.ckpt")
+        print("Model saved in file: %s" % save_path)
 
         # TODO: Save inference data using helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
